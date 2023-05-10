c3c09517066394fb2e5b71ea01d418389affed91
Aleksey Efimov <smaylik121@gmail.com>
2023-05-10 19:02:53 +0300

    Initial Commit

---
+++ StateAndDataFlow/RegisterView.swift
@@ -0,0 +1,46 @@
+//
+//  RegisterView.swift
+//  StateAndDataFlow
+//
+//  Created by Alexey Efimov on 14.09.2022.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State private var name = ""
+    @EnvironmentObject private var user: UserManager
+    
+    var body: some View {
+        VStack {
+            HStack {
+                TextField("Enter your name", text: $name)
+                    .multilineTextAlignment(.center)
+                Text("3")
+                    .padding()
+                    
+            }
+    
+            
+            Button(action: registerUser) {
+                HStack {
+                    Image(systemName: "checkmark.circle")
+                    Text("OK")
+                }
+            }
+        }
+    }
+    
+    private func registerUser() {
+        if !name.isEmpty {
+            user.name = name
+            user.isRegister.toggle()
+        }
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
